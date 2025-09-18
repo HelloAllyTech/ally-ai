5ed39606dc68d6df284def0396aae2b8468ce935
risal-kv <119860563+risal-kv@users.noreply.github.com>
2025-09-18 23:45:19 +0530

    Exceptions refactoring

--- app/exceptions/custom_exceptions.py
+++ app/exceptions/custom_exceptions.py
@@ -33,6 +33,19 @@ class ConversationAnalysisFailedException(BaseCustomException):
         super().__init__(message, status_code)
 
 
+class ConversationIdentifyFailedException(BaseCustomException):
+    """
+    Raised when conversation identify fails.
+    """
+
+    def __init__(
+        self,
+        message="Conversation identify failed",
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+    ) -> None:
+        super().__init__(message, status_code)
+
+
 class SummarizationFailedException(BaseCustomException):
     """
     Raised when summarization fails.
@@ -244,13 +257,13 @@ class CoreAPIFailedException(BaseCustomException):
 
 
 class CounselorTrainingAnalysisFailedException(BaseCustomException):
-   """
-   Raised when counselor training analysis generation fails.
-   """
+    """
+    Raised when counselor training analysis generation fails.
+    """
 
-   def __init__(
-       self,
-       message="Failed to generate counselor training analysis",
-       status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-   ) -> None:
-       super().__init__(message, status_code)
+    def __init__(
+        self,
+        message="Failed to generate counselor training analysis",
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+    ) -> None:
+        super().__init__(message, status_code)
